d588b7b9cf557a533b232785c8804dbf7c5e6501
zaki <zihali91@gmail.com>
2019-04-16 00:21:58 +0100

    Successfully mapped java models to database using ActiveJDBC within Maven build.

--- src/main/java/models/Main.java
+++ src/main/java/models/Main.java
@@ -1,16 +1,23 @@
 package models;
 
+import org.javalite.activejdbc.Base;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static com.sun.activation.registries.LogSupport.log;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../views/gui/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
@@ -18,6 +25,35 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+
         launch(args);
+        try {
+            // The newInstance() call is a work around for some
+            // broken Java implementations
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            log("seems like the driver is registered!");
+            Connection myConn = DriverManager.getConnection("JDBC:mysql://remotemysql.com:3306/dTXt3FVdSy",
+                    "dTXt3FVdSy", "s4dL5PTH35");
+            if (myConn != null) {
+                log("connection: success!");
+            }
+            else {
+                log("Connection: failed!");
+            }
+
+        } catch (SQLException | ClassNotFoundException e) {
+            log("mySQL connection failed!");
+            e.printStackTrace();
+            return;
+            // handle the error
+        }
+
+        Base.open();
+        Users u = new Users();
+        u.set("firstname", "Zak");
+        u.set("lastname", "Ali");
+        u.set("username", "zakiali");
+        u.saveIt();
     }
 }
--- src/main/java/models/Students.java
+++
@@ -1,5 +0,0 @@
-package models;
-
-import org.javalite.activejdbc.Model;
-
-public class Students extends Model {}
