9ffc45f325879d043e4b8447c38b6b7525391eb5
idris <idris_omer@hotmail.com>
2019-04-21 00:42:39 +0100

    Merge branch 'master' of C:\Users\idris\IdeaProjects\CSCM94Go with conflicts.

--- src/main/java/models/Leaderboard.java
+++ src/main/java/models/Leaderboard.java
@@ -1,4 +1,51 @@
 package models;
 
-public class Leaderboard {
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+@Table("leaderboard")
+
+public class Leaderboard extends Model {
+    String username;
+    int wins, losses;
+    float winlossPercentage;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
+
+    public float getWinlossPercentage() {
+        return winlossPercentage;
+    }
+
+    public void setWinlossPercentage(float winlossPercentage) {
+        this.winlossPercentage = winlossPercentage;
+    }
+
+    public  Leaderboard(String username, int wins, int losses, float winlossPercentage){
+        this.username = username;
+        this.wins = wins;
+        this.losses = losses;
+        this.winlossPercentage = winlossPercentage;
+
+    }
 }
