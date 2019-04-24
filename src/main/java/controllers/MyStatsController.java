a1fa5eddcc581ef2207516e25999b0139df72c15
hectorvalsan <hectorvalsan@gmail.com>
2019-04-24 22:22:17 +0100

    MyStatsController draft.

--- src/main/java/controllers/MyStatsController.java
+++ src/main/java/controllers/MyStatsController.java
@@ -1,10 +1,116 @@
 package controllers;
+import application.Main;
+import com.jfoenix.controls.JFXButton;
+import com.sun.org.apache.xml.internal.utils.res.StringArrayWrapper;
+import helpers.HelperMethods;
+import javafx.beans.property.ReadOnlyBooleanWrapper;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
-public class MyStatsController {
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.CheckBoxTableCell;
+
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import models.Admin;
+import models.Users;
+import org.json.JSONObject;
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.Date;
+import java.awt.*;
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+/**
+ * This class can ...
+ * @author Hector
+ * @version 1.0
+ */
+public class MyStatsController implements Initializable {
+    /**
+     * This
+     *
+     */
+    private static MyStatsController instance;
+    /**
+     * This
+     *
+     */
+    public MyStatsController() {
+        instance = this;
+    }
+
+
+    public static MyStatsController getInstance()
+    {
+        return instance;
+    }
+
+
+
+    @FXML
+    private TableView tableView;
+
+    @FXML
+    private JFXButton backtoDashboard;
+
+
+    @FXML
+    private ImageView userImage;
+
+    @FXML
+    private TextField user_name;
+
+    @FXML
+    private TextField last_name;
+
+    @FXML
+    private TextField first_name;
+
+
+    @FXML
+    void handleDashboardButtonAction(ActionEvent event) throws IOException {
+        HelperMethods.LoadScene("/fxml/HomeDashboard.fxml");
+    }
 
 
 
 
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+
+
+    }
 
 
 }
